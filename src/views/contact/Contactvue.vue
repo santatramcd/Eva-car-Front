ef1a602f2de6ad77dcee0fde0c6946be040e1f60
santatramcd <andrianjakasantatra22@gmail.com>
2025-01-17 14:56:01 +0300

    update contact maps

--- src/views/contact/Contactvue.vue
+++ src/views/contact/Contactvue.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.5863858985344!2d55.508906274512206!3d-4.670940448692087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x231fd5c71a3dddb1%3A0x798c3d195580eb02!2sA%C3%A9roport%20de%20Victoria-Seychelles!5e1!3m2!1sfr!2smg!4v1735622405542!5m2!1sfr!2smg" height="450" style="border:0; width:100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="container mt-5">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.5674019713265!2d55.50890627451217!3d-4.670940448692094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x231fd5c71a3dddb1%3A0x798c3d195580eb02!2sA%C3%A9roport%20de%20Victoria-Seychelles!5e0!3m2!1sfr!2smg!4v1737114572055!5m2!1sfr!2smg"
+        height="250"
+        style="border: 0; width: 100%"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
     <div class="container mt-5">
       <div class="row">
@@ -84,7 +91,6 @@
         </div>
       </div>
     </div>
-   
   </div>
 </template>
 <script setup></script>
